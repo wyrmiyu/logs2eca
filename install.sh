c629477b0e63d701262da2220162d31dbf088768
Sandi Wallendahl <wyrmiyu@gmail.com>
2023-06-16 06:14:17 +0300

    Slight refactor of install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+set -eo pipefail
 # Define default values
-SCRIPTS_LOCATION=$(pwd)
-INSTALL_PREFIX=/usr/local
-SYSTEMD_LOCATION=/etc/systemd/system
-ENV_LOCATION=/etc/logs2eca
+script_path="$(readlink -f -- $BASH_SOURCE)"
+script_dir="$(dirname $script_path)"
+
+src_dir="${LOGS2ECA_src_dir=$script_dir}"
+install_prefix="${LOGS2ECA_install_prefix:=/usr/local}"
+logs2eca_conf_dir="${LOGS2ECA_logs2eca_conf_dir:=/etc/logs2eca}"
+SYSTEMD_DIR=/etc/systemd/system
 RPM_DEPS=python3-inotify
 
 # Process command line options
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -s|--src) SCRIPTS_LOCATION="$2"; shift;;
-        -d|--dst) INSTALL_PREFIX="$2"; shift;;
+        -s|--src) src_dir="$2"; shift;;
+        -d|--dst) install_prefix="$2"; shift;;
         *) echo "Unknown parameter: $1"; exit 1;;
     esac
     shift
@@ -24,27 +27,27 @@ if ! rpm -q python3-inotify > /dev/null 2>&1; then
 fi
 
 # Install the logs2eca script
-install -m 755 -o root -g root ${SCRIPTS_LOCATION}/logs2eca ${INSTALL_PREFIX}/bin/logs2eca
+install -m 755 -o root -g root "${src_dir}/logs2eca" "${install_prefix}"/bin/logs2eca
 
 # Install the systemd service, replacing the ExecStart path with the install location
-sed "s|/usr/bin/logs2eca|${INSTALL_PREFIX}/bin/logs2eca|g" ${SCRIPTS_LOCATION}/logs2eca@.service > ${SYSTEMD_LOCATION}/logs2eca@.service
+sed "s|/usr/bin/logs2eca|${install_prefix}/bin/logs2eca|g" "${src_dir}/logs2eca@.service" > "${SYSTEMD_DIR}/logs2eca@.service"
 
 # Create the environment file directory if it doesn't exist
-if [ ! -d "$ENV_LOCATION" ]; then
-    mkdir -p $ENV_LOCATION
-    echo "Created directory ${ENV_LOCATION}..."
+if [ ! -d "$logs2eca_conf_dir" ]; then
+    mkdir -p "$logs2eca_conf_dir"
+    echo "Created directory ${logs2eca_conf_dir}..."
 fi
 
 # Install the environment file template
-install -m 644 -o root -g root ${SCRIPTS_LOCATION}/logs2eca_env_template ${ENV_LOCATION}/logs2eca_env_template
+install -m 644 -o root -g root "${src_dir}/logs2eca_env_template" "${logs2eca_conf_dir}/logs2eca_env_template"
 
 # Reload systemd daemon to reflect changes
 systemctl daemon-reload
 
 echo "Installation complete!"
 msg="
-Systemd service template: ${SYSTEMD_LOCATION}/logs2eca@.service
-Environment file template: ${ENV_LOCATION}/logs2eca_env_template
+Systemd service template: ${SYSTEMD_DIR}/logs2eca@.service
+Environment file template: ${logs2eca_conf_dir}/logs2eca_env_template
 
 When creating an instance of the logs2eca service, follow these steps:
 
@@ -53,7 +56,7 @@ When creating an instance of the logs2eca service, follow these steps:
     - Both the service and environment file must use the same name.
     - Replace <instance_name> with the name in the following steps.
 
-2. Copy the environment template to: ${ENV_LOCATION}/<instance_name>.monitor
+2. Copy the environment template to: ${logs2eca_conf_dir}/<instance_name>.monitor
     - Note that the file name must end with a '.monitor' extension.
     - E.g. 'sssd.monitor' or 'httpd_access_log.monitor'
     - Edit the file and fill in values for the following constants:
